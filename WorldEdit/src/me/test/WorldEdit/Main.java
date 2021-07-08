4aa4e5cdccd1e20383b4f01afa2cb7715ff01682
BradenProg <brado.dabest@hotmail.com>
2021-07-09 02:47:56 +1000

    Added positioning of blocks and filling, some directions do not work.
    Need to fix South and West
    Add ability to input own block
    Delete / Replace / Fill functionality

--- WorldEdit/src/me/test/WorldEdit/Main.java
+++ WorldEdit/src/me/test/WorldEdit/Main.java
@@ -3,11 +3,18 @@ package me.test.WorldEdit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.test.WorldEdit.commands.WorldEdit;
+import me.test.WorldEdit.commands.WorldEditCommands;
 
 public class Main extends JavaPlugin{
   
 	@Override
 	public void onEnable() {
-		new WorldEdit(this);
+//		WorldEditCommands commands = new WorldEditCommands(this);
+		this.getServer().getPluginManager().registerEvents(new WorldEdit(this), this);
+	}
+	
+	@Override
+	public void onDisable() {
+		
 	}
 }
